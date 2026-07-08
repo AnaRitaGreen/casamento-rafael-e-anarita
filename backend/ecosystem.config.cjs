ce07532c2879f558aed991dd1c3f6395123c51b5
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2026-07-08 16:41:52 -0300

    testando novamente o cjs

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
 
-export default {
+module.exports = {
   apps: [
     {
       name: 'casamento-rafael-e-anarita-api',
